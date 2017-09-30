ee6af4410aad7321be0cf540bb2d10467ded95ae
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2017-09-30 22:13:57 +0900

    SomeTable

---
+++ chap1/SomeTable.sql
@@ -0,0 +1,18 @@
+-- /* 条件を分岐させたUPDATE */
+-- CREATE TABLE SomeTable
+-- (p_key CHAR(1) PRIMARY KEY,
+--  col_1 INTEGER NOT NULL,
+--  col_2 CHAR(2) NOT NULL);
+--
+-- INSERT INTO SomeTable VALUES('a', 1, 'あ');
+-- INSERT INTO SomeTable VALUES('b', 2, 'い');
+-- INSERT INTO SomeTable VALUES('c', 3, 'う');
+
+-- psqlではエラー
+update SomeTable
+  set p_key = case
+      when p_key = 'a' then 'b'
+      when p_key = 'b' then 'a'
+      else p_key
+      end
+;
